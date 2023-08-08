3582509ff9aed52456bec4c1ba502d84dfdf6d17
taifun <laingawbl@gmail.com>
2023-08-08 16:30:47 -0700

    add 2 more tests for glue fns

--- tests/unit/test_glue.gd
+++ tests/unit/test_glue.gd
@@ -9,3 +9,17 @@ func test_string_conv_unicode():
 	var qb: String = FileAccess.get_file_as_string("res://tests/quickbrown.txt")
 	var rt = GlueTester.RoundTripConvert(qb)
 	assert_eq(qb, rt)
+
+func test_string_conv_astral():
+	var ap: String = '🌕𝐀𝓑𝛱𝟞👍🏼'
+	var rt = GlueTester.RoundTripConvert(ap)
+	assert_eq(ap, rt)
+	
+func test_config_map_basic():
+	var cf: ConfigFile = ConfigFile.new()
+	cf.set_value("foo", "bar", 12)
+	cf.set_value("baz", "qux", "hello")
+	cf.set_value("non", "existent", true)
+	cf.set_value("non", "existent", null)
+	GlueTester.ConvertAndPrintConfigFile(cf)
+	pass_test('It didn\'t throw anything great')
