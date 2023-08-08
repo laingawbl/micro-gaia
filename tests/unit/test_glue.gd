ea16dbb8059f26f101708b9ac594ee96bef94170
taifun <laingawbl@gmail.com>
2023-08-08 16:31:08 -0700

    gdformat

--- tests/unit/test_glue.gd
+++ tests/unit/test_glue.gd
@@ -1,20 +1,24 @@
 extends GutTest
 
+
 func test_string_conv_ascii():
 	var s: String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
 	var rt = GlueTester.RoundTripConvert(s)
 	assert_eq(s, rt)
-	
+
+
 func test_string_conv_unicode():
 	var qb: String = FileAccess.get_file_as_string("res://tests/quickbrown.txt")
 	var rt = GlueTester.RoundTripConvert(qb)
 	assert_eq(qb, rt)
 
+
 func test_string_conv_astral():
-	var ap: String = '🌕𝐀𝓑𝛱𝟞👍🏼'
+	var ap: String = "🌕𝐀𝓑𝛱𝟞👍🏼"
 	var rt = GlueTester.RoundTripConvert(ap)
 	assert_eq(ap, rt)
-	
+
+
 func test_config_map_basic():
 	var cf: ConfigFile = ConfigFile.new()
 	cf.set_value("foo", "bar", 12)
@@ -22,4 +26,4 @@ func test_config_map_basic():
 	cf.set_value("non", "existent", true)
 	cf.set_value("non", "existent", null)
 	GlueTester.ConvertAndPrintConfigFile(cf)
-	pass_test('It didn\'t throw anything great')
+	pass_test("It didn't throw anything great")
